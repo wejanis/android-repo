e0bce9137fd6b6feaa9bf625b07980a1b3c292b2
wejanis <will.janis@gmail.com>
2015-09-01 22:47:05 -0700

    [In work]: Added async task to RegisterActivity to make a post request with input data from the user. Updated JSONParser to handle GET/POST requests. [Complete]: Updated login, main, and register xml files to use hints in the input fields instead of having TextViews above them.

---
+++ app/src/main/java/com/wahnaton/testapp/testappli/JSONParser.java
@@ -0,0 +1,183 @@
+package com.wahnaton.testapp.testappli;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class JSONParser{
+
+   public JSONParser() {}
+
+    // function get json from url
+    // by making HTTP POST  method
+    public JSONObject makePostRequest(String url, HashMap<String, String> postDataParams) {
+
+        HttpURLConnection urlConnection = null;
+        String response ="";
+        JSONObject jObj = null;
+        // make HTTP request using POST method
+        try {
+            URL _url = new URL(url);
+
+            //setup connection
+            urlConnection = (HttpURLConnection) _url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setReadTimeout(15000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setUseCaches(false);
+
+            OutputStream os = new BufferedOutputStream(urlConnection.getOutputStream());
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            String postDataString = getPostDataString(postDataParams);
+            urlConnection.setFixedLengthStreamingMode(postDataString.getBytes().length);
+            bw.write(postDataString);
+            bw.flush();
+            bw.close();
+            os.close();
+
+            int responseCode= urlConnection.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                while ((line= br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(response);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+
+    //Formats the input of makePostRequest
+    private static String getPostDataString(HashMap<String, String> params){
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet())
+        {
+            if(first)
+                first = false;
+            else
+                result.append("&");
+
+            try {
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                throw new AssertionError("UTF-8 is unknown");
+            }
+            result.append("=");
+            try {
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                throw new AssertionError("UTF-8 is unknown");
+            }
+        }
+
+        return result.toString();
+    }
+
+
+    // function get json from url
+    // by making HTTP GET  method
+    public JSONObject makeGetRequest(String url) {
+
+        JSONObject jObj = null;
+        String json = "";
+        HttpURLConnection urlConnection = null;
+
+        //make HTTP request using GET method
+        try {
+            URL _url = new URL(url);
+            urlConnection = (HttpURLConnection) _url.openConnection();
+            urlConnection.setDoInput(true);
+            urlConnection.setReadTimeout(15000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setUseCaches(true);
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            StringBuilder sb = new StringBuilder();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+                sb.append(current);
+                System.out.print(current);
+            }
+            json = sb.toString();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+    }
+
+}
--- app/src/main/java/com/wahnaton/testapp/testappli/RegisterActivity.java
+++ app/src/main/java/com/wahnaton/testapp/testappli/RegisterActivity.java
@@ -1,17 +1,31 @@
 package com.wahnaton.testapp.testappli;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 
 public class RegisterActivity extends Activity implements View.OnClickListener {
+
+    private ProgressDialog pDialog;
+    private static String addUserUrl = "http://localhost/android_connection/addUser.php";
+    JSONParser jsonParser = new JSONParser();
 
     Button bRegister;
     EditText etName, etUsername, etPassword, etVerifyPassword;
@@ -26,20 +40,70 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         etVerifyPassword = (EditText) findViewById(R.id.etVerifyPassword);
-
         bRegister.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()){
-
             case R.id.bRegister:
-                startActivity(new Intent(this, LoginActivity.class));
+                new AddUser().execute();
                 break;
+        }
+    }
+
+    class AddUser extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(RegisterActivity.this);
+            pDialog.setMessage("Creating your account...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... args) {
+            String name = etName.getText().toString();
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+            String verifypassword = etVerifyPassword.getText().toString();
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("name", name);
+            params.put("username", username);
+            params.put("password", password);
+            params.put("verifypassword", verifypassword);
+
+            JSONObject json = jsonParser.makePostRequest(addUserUrl, params);
+            Log.d("Add user response: ", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt("success");
+                if (success == 1) {
+                    // successfully created product
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    pDialog.setMessage("Successfully created your account!");
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create product
+                    pDialog.setMessage("Failed to create account");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        protected void onPostExecute(String file_url){
+            pDialog.dismiss();
         }
 
 
     }
+
 }

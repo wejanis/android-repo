6f5ea406136a3d22e92897d1b6831e5999ca246f
wejanis <will.janis@gmail.com>
2015-09-24 23:05:28 -0700

    Added the action bar to the main activity. Removed the logout button and placed the logout functionality in the overflow menu on the action bar.

--- app/src/main/java/com/wahnaton/testapp/testappli/MainActivity.java
+++ app/src/main/java/com/wahnaton/testapp/testappli/MainActivity.java
@@ -1,49 +1,77 @@
 package com.wahnaton.testapp.testappli;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-// change to test git 2.0
-public class MainActivity extends Activity implements View.OnClickListener {
+import static com.wahnaton.testapp.testappli.R.menu.menu_main;
 
-    Button bLogout;
+// change to test git 2.0
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+
+   // Button bLogout;
     EditText etName, etUsername;
     private SecurePreferences loginPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        boolean finish = getIntent().getBooleanExtra("finish", false);
+        if (finish) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
 
         loginPrefs = new SecurePreferences(this, "user-info", "randomTestingPurposesKey", true);
 
-        bLogout = (Button) findViewById(R.id.bLogout);
         etName = (EditText) findViewById(R.id.etName);
         etUsername = (EditText) findViewById(R.id.etUsername);
 
         String username = loginPrefs.getString("username");
         etUsername.setText(username);
 
-        bLogout.setOnClickListener(this);
-
     }
 
-    @Override
-    public void onClick(View v) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        switch(v.getId()){
-            case R.id.bLogout:
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId()){
+
+            case R.id.action_settings:
+                return true;
+            case R.id.action_logout:
                 startActivity(new Intent(this, LoginActivity.class));
-                break;
+                finish();
+                //Activities aside from MainActivity need:
+                //Intent intent = new Intent(this, HomeActivity.class);
+                //intent.putExtra("finish", true);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // To clean up all activities
+                //startActivity(intent);
+                //finish();
+                return true;
 
-
+            default:
+                return super.onOptionsItemSelected(item);
 
         }
 
-
     }
+
 }

4102f52e1baf974c7d7f669662c9f550683060a3
wejanis <will.janis@gmail.com>
2015-09-28 23:02:03 -0700

    Added a libs folder for external jars. Imported external jar date4j to help deal with calendar dates. Added a PagerTitleStrip to the main activity to show the day as the title. Put a viewpager on the main activity so it can slide back and forth for each workout day. Added a generic layout xml file that applies to each day in the workout view. Added a date picker so the user can select a date from the date picker and have the main activity slide to that date. Calendar/title syncing is in work. Added username to action bar, which currently only works if the user selects Remember Me.

--- app/src/main/java/com/wahnaton/testapp/testappli/MainActivity.java
+++ app/src/main/java/com/wahnaton/testapp/testappli/MainActivity.java
@@ -1,34 +1,39 @@
 package com.wahnaton.testapp.testappli;
 
-import android.app.ActionBar;
-import android.app.ActivityOptions;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Locale;
+import java.util.TimeZone;
 
-import static com.wahnaton.testapp.testappli.R.menu.menu_main;
+import hirondelle.date4j.DateTime;
 
-// change to test git 2.0
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+public class MainActivity extends AppCompatActivity {
 
     private SecurePreferences loginPrefs;
-    private TextView tvDate;
-    private Calendar myCalendar;
-
+    //private TextView tvDate;
+    //private Calendar myCalendar;
+    private DateTime datePicked;
+    private Toolbar toolBarTop;
+    private ViewPager mPager;
+    private int numDays;
     private DatePickerDialog.OnDateSetListener date;
+    private static int NUM_PAGES = 5000;
+    private PagerTitleStrip pts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,24 +50,60 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
 
         loginPrefs = new SecurePreferences(this, "user-info", "randomTestingPurposesKey", true);
 
-        tvDate = (TextView) findViewById(R.id.tvDate);
+        toolBarTop = (Toolbar) findViewById(R.id.toolBarTop);
+        //tvDate = (TextView) toolBarTop.findViewById(R.id.tvDate);
+        mPager = (ViewPager) findViewById(R.id.mPager);
+        pts = (PagerTitleStrip) findViewById(R.id.tsPager);
+        mPager.setAdapter(new ScreenSlidePagerAdapter(getResources(), getSupportFragmentManager()));
+        mPager.setCurrentItem(NUM_PAGES / 2, false);
+
+        mPager.getAdapter().notifyDataSetChanged();
+        mPager.setOffscreenPageLimit(0);
+        pts.setNonPrimaryAlpha(0);
+
+
 
         String username = loginPrefs.getString("username");
         setTitle("Welcome, " + username + "!");
 
-        String myFormat = "MM/dd/yy";
+        final String myFormat = "MM/dd/yy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        tvDate.setText(sdf.format(Calendar.getInstance().getTime()));
 
+        datePicked = DateTime.now(TimeZone.getDefault());
+        numDays = mPager.getCurrentItem() - (NUM_PAGES/2);
 
-        myCalendar = Calendar.getInstance();
         date = new DatePickerDialog.OnDateSetListener() {
           public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
           {
-              myCalendar.set(Calendar.YEAR, year);
-              myCalendar.set(Calendar.MONTH, monthOfYear);
-              myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-              updateLabel();
+              numDays = mPager.getCurrentItem() - (NUM_PAGES/2);
+              System.out.println("original numdays: " + 0);
+
+              //DatePicker indexes the months from 0 (Jan.) - 11 (Dec.) so need to add one to the month parameter.
+              datePicked = DateTime.forDateOnly(year, monthOfYear + 1, dayOfMonth);
+              System.out.println("datePicked: " + datePicked.toString() + "------------------------------------------");
+
+
+              DateTime dt = DateTime.now(TimeZone.getDefault());  // dt = 9/27
+              System.out.println("dt before day adjustment: " + dt.toString() + "------------------------------------------");
+
+              if(numDays < 0) {
+                  System.out.println("current pos numdays from today < 0:" + numDays);
+                  dt = dt.minusDays(numDays); //
+              }
+              else if (numDays > 0) {
+                  dt = dt.plusDays(numDays); // dt = 9/29
+                  System.out.println("current pos numdays from today > 0:" + numDays);
+              }
+              else{
+                  System.out.println("current pos numdays from today = 0: " + numDays);
+              }
+
+
+              System.out.println("dt after day adjustment: " + dt.toString() + "------------------------------------------");
+              int num = dt.numDaysFrom(datePicked);
+              System.out.println("dt num days from datepicked: " + num + "------------------------------------------");
+              mPager.setCurrentItem(mPager.getCurrentItem() + num);
+              System.out.println("currPos: " + mPager.getCurrentItem() + "------------------------------------------");
           }
         };
 
@@ -92,7 +133,7 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
                 return true;
             case R.id.action_pickdate:
 
-                new DatePickerDialog(this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog(this, date, datePicked.getYear(), datePicked.getMonth()-1, datePicked.getDay()).show();
 
                 return true;
             default:
@@ -102,15 +143,59 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
 
     }
 
-
-    private void updateLabel() {
-
-        String myFormat = "MM/dd/yy";
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-        tvDate.setText(sdf.format(myCalendar.getTime()));
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
     }
 
+    private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
+        public ScreenSlidePagerAdapter(Resources resources, FragmentManager fm) {
+            super(fm);
+        }
+
+        public int getItemPosition(Object object){
+            return FragmentStatePagerAdapter.POSITION_NONE;
+        }
+
+        public Fragment getItem(int position){
+
+            ScreenSlidePageFragment sspFragment = new ScreenSlidePageFragment();
+
+            return sspFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            CharSequence cs;
+
+            DateTime pagerdate = DateTime.now(TimeZone.getDefault());
+            DateTime days = pagerdate.plusDays(position - (NUM_PAGES/2));
+
+            if(position - (NUM_PAGES/2) == 0)
+                cs = "Today";
+            else if (position - (NUM_PAGES/2) == 1)
+                cs = "Tomorrow";
+            else if (position - (NUM_PAGES/2) == -1)
+                cs = "Yesterday";
+            else
+                cs = days.format("WWWW, MM/DD/YYYY", Locale.getDefault()).toString();
+
+            return cs;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/wahnaton/testapp/testappli/ScreenSlidePageFragment.java
@@ -0,0 +1,19 @@
+package com.wahnaton.testapp.testappli;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+
+}

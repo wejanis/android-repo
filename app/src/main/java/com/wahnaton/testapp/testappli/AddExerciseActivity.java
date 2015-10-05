aedc949c5083da268cf2dc6fb5842f8ca2d50b8d
wejanis <will.janis@gmail.com>
2015-10-04 23:11:02 -0700

    Made the add exercise activity a list view populated by a hardcoded string array of exercises (probably will change to an external text file in the future so the exercises aren't hardcoded). Added a search filter to the list of exercises.

--- app/src/main/java/com/wahnaton/testapp/testappli/AddExerciseActivity.java
+++ app/src/main/java/com/wahnaton/testapp/testappli/AddExerciseActivity.java
@@ -1,15 +1,49 @@
 package com.wahnaton.testapp.testappli;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
-public class AddExerciseActivity extends ActionBarActivity {
+public class AddExerciseActivity extends Activity {
 
+    private EditText etSearch;
+    private ListView exerciseListView;
+    private ListAdapter exerciseAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+        setTitle("");
+
+        String[] exercises = {"blah", "aaaa", "yo", "etc", "asdf", "ttttt", "123", "123", "123", "123", "123", "123", "123", "123", "123", "123"};
+        exerciseAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, exercises);
+
+        exerciseListView = (ListView) findViewById(android.R.id.list);
+        exerciseListView.setAdapter(exerciseAdapter);
+
+        etSearch = (EditText) findViewById(R.id.etSearch);
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ((ArrayAdapter<String>) AddExerciseActivity.this.exerciseAdapter).getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+
+
     }
 
 }

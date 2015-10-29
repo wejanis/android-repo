f7f24c84566727249462bdd4277e49bbb6e76428
wejanis <will.janis@gmail.com>
2015-10-28 23:22:15 -0700

    Added weight/reps members so they could be set/extracted independently of the exercise details member.

--- app/src/main/java/com/wahnaton/testapp/testappli/ExerciseSetModel.java
+++ app/src/main/java/com/wahnaton/testapp/testappli/ExerciseSetModel.java
@@ -6,13 +6,18 @@ public class ExerciseSetModel {
     private String exerciseName;
     private String exerciseDetails;
     private int isComplete;
+    private String weight;
+    private String reps;
 
-    public ExerciseSetModel(int exerciseId, String exerciseName, String exerciseDetails, int isComplete) {
+    public ExerciseSetModel(int exerciseId, String exerciseName, String weight, String reps, int isComplete) {
 
         this.exerciseId = exerciseId;
         this.exerciseName = exerciseName;
-        this.exerciseDetails = exerciseDetails;
+        this.weight = weight;
+        this.reps = reps;
         this.isComplete = isComplete;
+
+        exerciseDetails = weight + "lbs, " + reps + " rep(s)";
     }
 
     public int getExerciseId(){
@@ -24,8 +29,28 @@ public class ExerciseSetModel {
         return this.exerciseName;
     }
 
+    public String getWeight(){
+        return this.weight;
+    }
+
+    public void setWeight(String weight){
+        this.weight = weight;
+    }
+
+    public String getReps(){
+        return this.reps;
+    }
+
+    public void setReps(String reps){
+        this.reps = reps;
+    }
+
     public String getExerciseDetails(){
         return this.exerciseDetails;
+    }
+
+    public void setExerciseDetails(String weight, String reps){
+        this.exerciseDetails = weight + "lbs, " + reps + " rep(s)";
     }
 
     public int getIsComplete(){
